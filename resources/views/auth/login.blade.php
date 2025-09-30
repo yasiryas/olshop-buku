2adf6674f6b58f2c4b8dfedeeddd5c5a8ad1fba2
Yasir Yas <yasir123983@gmail.com>
2025-09-30 19:26:21 +0700

    Add pesan error pada login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,6 +26,9 @@
                         style="background-image: url('/assets/svgs/ic-email.svg');"
                         class="form-input bg-[url('/public/assets/svgs/ic-email.svg')]" placeholder="Your email address"
                         :value="old('email')" required autofocus autocomplete="username">
+                    @error('email')
+                        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <!-- Password -->
                 <div class="flex flex-col gap-2.5">
@@ -34,10 +37,12 @@
                         style="background-image: url('/assets/svgs/ic-lock.svg');"
                         class="form-input bg-[url('/public/assets/svgs/ic-lock.svg')]"
                         placeholder="Protect your password" autocomplete="current-password">
+                    @error('password')
+                        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <button type="submit"
-                    class="inline-flex text-white font-bold text-base bg-primary rounded-full whitespace-nowrap px-[30px] py-3 justify-center items-center"
-                    onclick="window.location.href='/public/pages/index.html'">
+                    class="inline-flex text-white font-bold text-base bg-primary rounded-full whitespace-nowrap px-[30px] py-3 justify-center items-center">
                     Sign In
                 </button>
             </div>
